5c81b52eeeac35d6c1290d0ed50cc1160bf9ae50
hiba.js <hiba.js@MEMLT0037>
2026-01-05 01:00:15 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace CsharpProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number");
+            float number1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number");
+            float number2 = float.Parse(Console.ReadLine());
+
+            float result = number1 + number2;
+                Console.WriteLine("The result:"+ result);
+          
+        if (result>50)
+            {
+                Console.WriteLine("Passed");
+                    }
+
+    else
+            { Console.WriteLine("Faild");
+            }
+            int counter;
+            for (counter = 1; counter <= 5; counter++) ;
+        }
+    }
+}
---
+++ TASK1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter how many numbers you want:");
+            int n = int.Parse(Console.ReadLine());
+
+            float sum = 0;
+            int count = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("Enter number " + i + ":");
+                float number = float.Parse(Console.ReadLine());
+
+                sum = sum + number;
+                count = count + 1;
+            }
+
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Count = " + count);
+
+        }
+    }
+}
